0e4e0aed0546482e1e32ad98496404e297d7c0f2
Alexandr <ixarka@mail.ru>
2017-11-13 09:17:55 +0300

    Grouping Classes

--- Project_text/Project_text/Program.cs
+++ Project_text/Project_text/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 
 namespace Project_text
@@ -23,25 +24,25 @@ namespace Project_text
             listSentences = r.Read();
             var text = parser.Parse(listSentences);
 
-            ///1 Вывести все предложения заданного текста в порядке возрастания количества слов в каждом из них.
+            //1 Вывести все предложения заданного текста в порядке возрастания количества слов в каждом из них.
             foreach (var item in text.SortSentences())
             {
                 Console.WriteLine(item);
             }
             Console.WriteLine(line);
 
-            ///2 Во всех вопросительных предложениях текста найти и напечатать без повторений слова заданной длины.
+            //2 Во всех вопросительных предложениях текста найти и напечатать без повторений слова заданной длины.
             text.FindWordsOfPredeterminedLenght(text, 4);
             Console.WriteLine(line);
 
-            ///3 Из текста удалить все слова заданной длины, начинающиеся на согласную букву.
+            //3 Из текста удалить все слова заданной длины, начинающиеся на согласную букву.
             text.RemoveWords(2);
             Console.WriteLine(text);
             Console.WriteLine(line);
 
-            ///4 В некотором предложении текста слова заданной длины заменить указанной подстрокой, 
+            //4 В некотором предложении текста слова заданной длины заменить указанной подстрокой, 
             ///длина которой может не совпадать с длиной слова.
-            text.ReplaceWords(2, 2, "Vasya Petrov");
+            text.ReplaceWords(2, 2, "Test test");
             Console.WriteLine(text);
             Console.WriteLine(line);
 
